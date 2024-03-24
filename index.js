2c320785c16249d861a44fd51c35f58d40628193
Shriram <shriramrayakar@gmail.com>
2024-03-24 22:19:10 +0530

    third

--- index.js
+++ index.js
@@ -29,12 +29,11 @@ var allowCrossDomain = function(req, res, next) {
 
     const supabase = createClient.createClient("https://jeqafxlpwbyxgqfeufjz.supabase.co", 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImplcWFmeGxwd2J5eGdxZmV1Zmp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA4Njg1MzcsImV4cCI6MjAyNjQ0NDUzN30.m3lk4-fjePT5TxzQpHN_iX9TvIu6-HPcnVmtPoZmsE4');
 
-app.post('/check', async (req, res)=> {
+app.post('/creatorReg', async (req, res)=> {
   
   console.log(req.body);
   const body = req.body;
-  mail.mail(body.email);
-  /*
+  mail.mail(body.email, false);
   const { data, error } = await supabase
   .from('Creators')
   .insert([{email: body.email, name: body.name, insta_id: body.insta, ph_no: body.phone, category: body.category} ])
@@ -46,23 +45,44 @@ if (error){
 } else{
   console.log(data);
 } 
-  */
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', true);
-  /*  
-  axios.get(`https://i.instagram.com/api/v1/users/web_profile_info/?username=${req.body.insta}`, {
-    headers: {
-      'User-Agent' : 'Instagram 76.0.0.15.395 Android (24/7.0; 640dpi; 1440x2560; samsung; SM-G930F; herolte; samsungexynos8890; en_US; 138226743)',
-      'Origin': "https://www.instagram.com", 
-      'Referer': "https://www.instagram.com/",
-      'Access-Control-Allow-Origin': "*"
-    }
-  }).then(({data})=>{
-    console.log(data.data.user.edge_followed_by.count);
-    res.status(200).json(data.data.user.edge_followed_by.count);
-  }); */
+  res.status(200).json('success');
+})
+
+app.post('/brandReg', async (req, res)=> {
+  
+  console.log(req.body);
+  const body = req.body;
+  const { data, error } = await supabase
+  .from('Brand')
+  .insert([{email: body.email, brand: body.brand, insta_id: body.insta, contact_no: body.phone, target_audience: body.targetAudience, budget_range: body.budgetRange} ])
+  .select()
+          
+if (error){
+  console.log(error);
+  res.status(500).json('failure');
+} else{
+  console.log(data);
+} 
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.status(200).json('success');
+})
+
+app.post('/contact', async (req, res)=> {
+  
+  console.log(req.body);
+  const body = req.body;
+  mail.mail(body.email, body.message);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   res.status(200).json('success');
 })
 
--- mail.js
+++ mail.js
@@ -1,6 +1,6 @@
 var nodemailer = require('nodemailer');
 
-function mail(email) {
+function mail(email, content) {
 
     var transporter = nodemailer.createTransport({
         host: "smtp-relay.brevo.com",
@@ -13,9 +13,9 @@ function mail(email) {
     });
     
     var mailOptions = {
-      from: 'youremail@gmail.com',
-      to: email,
-      subject: 'Sending Email using Node.js',
+      from: content ? 'contactSupport@gmail.com': 'elevanceclub@gmail.com',
+      to: content ? 'elevanceclub@gmail.com' : email ,
+      subject: content ? 'Query from a customer': `Thank you for registering! Here's what you need to do next!`,
       text: `Welcome to Elevance Club!
 
       Dear Creator,
@@ -33,6 +33,7 @@ function mail(email) {
       
       
       NOTE: THE CREATOR MUST USE ELEVANCE BRANDING FOR EVERY POST IN ORDER TO BE ELIGIBLE FOR MONTHLY REWARDS.
+
       
       
       It's designed to help you showcase our brand effectively while staying true to your unique style.
@@ -41,23 +42,25 @@ function mail(email) {
       
       Best regards,
       Elevance Team`,
-      html: `<h2>Welcome to Elevance Club!</h2>
+      html: content ? ` ${content} <br/> from Client Email: ${email}` :`<h2>Welcome to Elevance Club!</h2>
 
       Dear Creator, <br />
       
-      <br />&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspThank you for registering yourself as an Elevance creator! We're thrilled to have you on board and look forward to seeing your creativity shine. <br /><br />
+      <br />Thank you for registering yourself as an Elevance creator! We're thrilled to have you on board and look forward to seeing your creativity shine. <br /><br />
       
-      &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspJust a friendly reminder: please ensure that your Instagram account is set to public to fully participate in Elevance Club's activities and opportunities.<br /><br />
+      Just a friendly reminder: please ensure that your Instagram account is set to public to fully participate in Elevance Club's activities and opportunities.<br /><br />
       
       Additionally, we encourage you to utilize:<br />
       • Elevance Media Kit<br />
       • The Hashtag #ElevanceClub<br />
       (ALL MANDATORY)<br /><br />
       
-      It's advised to see the "how to use elevance media kit" below to know more.
+      <b>The link of the media kit and other instructions are placed below.</b>
       
       
-      <h4>NOTE: THE CREATOR MUST USE ELEVANCE BRANDING FOR EVERY POST IN ORDER TO BE ELIGIBLE FOR MONTHLY REWARDS</h4>
+      <h4>NOTE: THE CREATOR MUST USE ELEVANCE BRANDING FOR EVERY POST IN ORDER TO BE ELIGIBLE FOR MONTHLY REWARDS. 
+      <a href='https://drive.google.com/file/d/12EpXY0U-Q63PTjeB65CusOTww9ymojwC/view?usp=drive_link'>Click here</a> to download the Elevance Media Kit.
+      </h4>
       
       
       It's designed to help you showcase our brand effectively while staying true to your unique style.<br /><br />
